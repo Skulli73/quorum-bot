d4d8d5c935dd0250d52b4b58713df52f0f42032b
Skulli73 <skulli730@gmail.com>
2022-12-01 21:06:43 +0100

    FInished /config show

--- src/main/java/io/github/Skulli73/Main/commands/ConfigShowCommand.java
+++ src/main/java/io/github/Skulli73/Main/commands/ConfigShowCommand.java
@@ -1,9 +1,12 @@
 package io.github.Skulli73.Main.commands;
 
 import io.github.Skulli73.Main.listeners.SlashCommandListener;
+import io.github.Skulli73.Main.objects.VoteWeight;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.interaction.SlashCommandInteraction;
+
+import java.text.DecimalFormat;
 
 public class ConfigShowCommand extends CouncilCommand{
     public ConfigShowCommand(SlashCommandInteraction pInteraction, DiscordApi pApi) {
@@ -13,7 +16,13 @@ public class ConfigShowCommand extends CouncilCommand{
     @Override
     public void executeCommand(SlashCommandInteraction pInteraction, DiscordApi pApi) {
         EmbedBuilder lEmbedBuilder      = new EmbedBuilder();
-        lEmbedBuilder.addField("Floor Channel", "<#" + council.floorChannel + ">")
+        StringBuilder lVoteWeights      = new StringBuilder();
+        DecimalFormat lFormat = new DecimalFormat("0.#");
+        for(VoteWeight lVoteWeight : council.voteWeightArrayList) {
+            lVoteWeights.append(pApi.getRoleById(lVoteWeight.roleId).get().getMentionTag()).append(": ").append(lFormat.format(lVoteWeight.votes)).append("\n");
+        }
+        lEmbedBuilder.setTitle(council.getName() + " Configurations")
+                .addField("Floor Channel", "<#" + council.floorChannel + ">")
                 .addField("Agenda Channel", "<#" + council.agendaChannel + ">")
                 .addField("Minute Channel", "<#" + council.minuteChannel + ">")
                 .addField("Legislation Channel", "<#" + council.legislationChannel + ">")
@@ -24,6 +33,8 @@ public class ConfigShowCommand extends CouncilCommand{
                 .addField("Standard Type of Majority for Introduction", SlashCommandListener.lTypeOfMajorityArray[council.firstReadingTypeOfMajority])
                 .addField("Amendment Majority", council.amendmentMajority*100 + "%")
                 .addField("Type of Majority for Amendments", SlashCommandListener.lTypeOfMajorityArray[council.amendmentTypeOfMajority])
+                .addField("Motion Timeout Time", lFormat.format(council.timeOutTime) + "h")
+                .addField("Vote Weights", lVoteWeights.toString())
         ;
         pInteraction.createImmediateResponder().addEmbed(lEmbedBuilder).respond();
     }
