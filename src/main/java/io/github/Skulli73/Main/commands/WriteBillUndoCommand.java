3507e9a1ae42616f47bb09d1f759f8b5081f49a3
TAMaurice <schreiner.maurice@outlook.com>
2022-12-27 12:07:42 +0100

    Improved readability, improved nested-if and if-else restructuring

--- src/main/java/io/github/Skulli73/Main/commands/WriteBillUndoCommand.java
+++ src/main/java/io/github/Skulli73/Main/commands/WriteBillUndoCommand.java
@@ -15,29 +15,37 @@ public class WriteBillUndoCommand extends WriteBillCommand{
     @Override
     public void executeCommand(SlashCommandInteraction pInteraction, DiscordApi pApi) {
         Bill lBill = bills.get(pInteraction.getOptions().get(0).getOptionStringValueByName("bill_message_id").get());
-        boolean b = false;
-        if(lBill.partArrayList.size() > 1)
-            b = true;
+        boolean b = lBill.partArrayList.size() > 1;
         Part lPart = lBill.partArrayList.get(lBill.partArrayList.size()-1);
+
         if(lPart.divisionArrayList.size() > 1 || lPart.divisionArrayList.get(lPart.divisionArrayList.size()-1).sectionArrayList.size() > 1) {
             Division lDivision = lPart.divisionArrayList.get(lPart.divisionArrayList.size()-1);
-            if(lDivision.sectionArrayList.size() > 1) {
-                Section lSection = lDivision.sectionArrayList.get(lDivision.sectionArrayList.size()-1);
-                if(lSection.subSectionArrayList.size() > 0) {
-                    SubSection lSubSection = lSection.subSectionArrayList.get(lSection.subSectionArrayList.size()-1);
-                    if(lSubSection.hasSubSections()) {
-                        lSubSection.deleteLowestSubSection();
-                    } else
-                        lSection.subSectionArrayList.remove(lSection.subSectionArrayList.size()-1);
-                    lSection.subSectionArrayList.set(lSection.subSectionArrayList.size()-1, lSubSection);
-                } else
-                    lDivision.sectionArrayList.remove(lDivision.sectionArrayList.size()-1);
-                lPart.divisionArrayList.set(lPart.divisionArrayList.size()-1,lDivision);
-            } else
+            if(!(lDivision.sectionArrayList.size() > 1)) {
                 lPart.divisionArrayList.remove(lPart.divisionArrayList.size()-1);
+                return;
+            }
+
+            Section lSection = lDivision.sectionArrayList.get(lDivision.sectionArrayList.size()-1);
+            if(!(lSection.subSectionArrayList.size() > 0)) {
+                lDivision.sectionArrayList.remove(lDivision.sectionArrayList.size() - 1);
+                return;
+            }
+
+            SubSection lSubSection = lSection.subSectionArrayList.get(lSection.subSectionArrayList.size()-1);
+            if(!lSubSection.hasSubSections()) {
+                lSection.subSectionArrayList.remove(lSection.subSectionArrayList.size()-1);
+                return;
+            }
+
+            lSubSection.deleteLowestSubSection();
+            lSection.subSectionArrayList.set(lSection.subSectionArrayList.size()-1, lSubSection);
+
+            lPart.divisionArrayList.set(lPart.divisionArrayList.size()-1,lDivision);
+
+
             lBill.partArrayList.set(lBill.partArrayList.size()-1, lPart);
-        } else if(b)
-            lBill.partArrayList.remove(lBill.partArrayList.size()-1);
+        } else if(b) lBill.partArrayList.remove(lBill.partArrayList.size()-1);
+
         bills.put(Long.toString(lBill.messageId), lBill);
         lBill.update();
         saveBills();
