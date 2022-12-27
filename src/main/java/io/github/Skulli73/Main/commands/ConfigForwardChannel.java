0f324f2db070bb445386400d7ca4f3c3a82c1097
TAMaurice <schreiner.maurice@outlook.com>
2022-12-27 11:16:46 +0100

    Improved readability

--- src/main/java/io/github/Skulli73/Main/commands/ConfigForwardChannel.java
+++ src/main/java/io/github/Skulli73/Main/commands/ConfigForwardChannel.java
@@ -19,19 +19,9 @@ public class ConfigForwardChannel extends ConfigCommand{
     public void changeConfig(SlashCommandInteraction pInteraction, DiscordApi pApi) {
         configName = "forward_channel";
         Optional<ServerChannel> lForwardCouncilOptional = pInteraction.getOptions().get(0).getOptionChannelValueByName("channel");
-        if(lForwardCouncilOptional.isPresent()) {
-            if(lForwardCouncilOptional.get().getType().isTextChannelType()) {
-                if(Council.isChannelFloor(lForwardCouncilOptional.get().asTextChannel().get(), councils)) {
-                    Council lForWardCouncil = Council.councilByFloorChannel(lForwardCouncilOptional.get().asTextChannel().get(), councils);
-                    council.forwardCouncil = Math.toIntExact(lForWardCouncil.getId());
-                    lForWardCouncil.legislationChannel = council.legislationChannel;
-                    lForWardCouncil.forwardCouncil = Math.toIntExact(council.getId());
-                    lForWardCouncil.isForwardCouncil = true;
-                    councils.set((int) lForWardCouncil.getId(), lForWardCouncil);
-                } else
-                    pInteraction.getChannel().get().sendMessage("You did not mention a Floor Channel");
-            }
-        } else {
+
+        // Check if council to forward to exists
+        if(lForwardCouncilOptional.isEmpty()) {
             Council lCouncil = council.getForwardCouncil();
             council.forwardCouncil = null;
             lCouncil.forwardCouncil = null;
@@ -39,6 +29,20 @@ public class ConfigForwardChannel extends ConfigCommand{
             councils.set((int) council.getId(), council);
             councils.set((int) lCouncil.getId(), lCouncil);
             saveCouncil(lCouncil);
+            return;
+        }
+
+        // Forwards the motion
+        if(lForwardCouncilOptional.get().getType().isTextChannelType()) {
+            if(Council.isChannelFloor(lForwardCouncilOptional.get().asTextChannel().get(), councils)) {
+                Council lForWardCouncil = Council.councilByFloorChannel(lForwardCouncilOptional.get().asTextChannel().get(), councils);
+                council.forwardCouncil = Math.toIntExact(lForWardCouncil.getId());
+                lForWardCouncil.legislationChannel = council.legislationChannel;
+                lForWardCouncil.forwardCouncil = Math.toIntExact(council.getId());
+                lForWardCouncil.isForwardCouncil = true;
+                councils.set((int) lForWardCouncil.getId(), lForWardCouncil);
+            } else
+                pInteraction.getChannel().get().sendMessage("You did not mention a Floor Channel");
         }
     }
 }
