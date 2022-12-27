285793d43fd864c36c22e129d8c54019de5f0ba6
TAMaurice <schreiner.maurice@outlook.com>
2022-12-27 11:42:45 +0100

    Improved readability, improved null protections and nested-if restructuring

--- src/main/java/io/github/Skulli73/Main/commands/WriteAmendmentAdd.java
+++ src/main/java/io/github/Skulli73/Main/commands/WriteAmendmentAdd.java
@@ -21,70 +21,79 @@ public class WriteAmendmentAdd extends WriteAmendmentCommand{
         String lAmendmentTitle = lSlashCommandInteractionOption.getOptionStringValueByName("title").get();
         String lAmendmentText = lSlashCommandInteractionOption.getOptionStringValueByName("text").get();
         int lPartId = Math.toIntExact(lSlashCommandInteractionOption.getOptionLongValueByName("part_id").get());
-        if(lPartId < bill.partArrayList.size()) {
-            System.out.println("Part Id: " + lPartId);
-            if(lSlashCommandInteractionOption.getOptionLongValueByName("division_id").isPresent()) {
-                int lDivisionId = Math.toIntExact(lSlashCommandInteractionOption.getOptionLongValueByName("division_id").get());
-                System.out.println("Division Id: " + lDivisionId);
-                    if(lDivisionId < bill.partArrayList.get(lPartId).divisionArrayList.size()) {
-                        if(lSlashCommandInteractionOption.getOptionLongValueByName("section_id").isPresent()) {
-                            int lSectionId = Math.toIntExact(lSlashCommandInteractionOption.getOptionLongValueByName("section_id").get());
-                            System.out.println("Section Id: " + lSectionId);
-                            if(lDivisionId < bill.partArrayList.get(lPartId).divisionArrayList.get(lDivisionId).sectionArrayList.size()) {
-                                if(lSlashCommandInteractionOption.getOptionStringValueByName("sub_section_id").isPresent()) {
-                                    String lSubSectionIdString = lSlashCommandInteractionOption.getOptionStringValueByName("sub_section_id").get();
-                                    System.out.println("Sub-Section Id as String: " + lSubSectionIdString);
-                                    String[] lSubSectionIdStringArray = lSubSectionIdString.split("\\.");
-                                    System.out.println("Sub-Section Id as Array: " + Arrays.toString(lSubSectionIdStringArray));
-                                    int[] lSubSectionIdIntArray = new int[lSubSectionIdStringArray.length];
-                                    boolean lValid = true;
-                                    for(int i = 0; i<lSubSectionIdStringArray.length; i++) {
-                                        try {
-                                            lSubSectionIdIntArray[i] = Integer.parseInt(lSubSectionIdStringArray[i])-1;
-                                        }
-                                        catch (NumberFormatException | NullPointerException e) {
-                                            lValid = false;
-                                        }
-                                    }
-                                    if(lValid) {
-                                        if(lSubSectionIdIntArray[0] < bill.partArrayList.get(lPartId).divisionArrayList.get(lDivisionId).sectionArrayList.get(lSectionId).subSectionArrayList.size()) {
-                                            SubSection lSubSection  = bill.partArrayList.get(lPartId).divisionArrayList.get(lDivisionId).sectionArrayList.get(lSectionId).subSectionArrayList.get(lSubSectionIdIntArray[0]);
-                                            boolean lValid2 = true;
-                                            for (int i = 1; i < lSubSectionIdIntArray.length &&lValid2; i++) {
-                                                if (lSubSection.subSectionArrayList.size() <= lSubSectionIdIntArray[i]) {
-                                                    pInteraction.createImmediateResponder().append("Not a valid Sub-Section.\nError Code: 02").respond();
-                                                    lValid2 = false;
-                                                } else
-                                                    lSubSection = lSubSection.subSectionArrayList.get(lSubSectionIdIntArray[i]);
 
-                                            }
-                                            if(lValid2) {
-                                                 amendment.additions.add(new String[]{lPartId + "." + lDivisionId + "." + lSectionId + "." + lSubSectionIdString, lAmendmentText, lAmendmentTitle});
-                                            }
-                                        } else pInteraction.createImmediateResponder().append("Not a valid Sub-Section.\nError Code: 01").respond();
-                                    }
-                                } else
-                                    amendment.additions.add(new String[]{lPartId + "." + lDivisionId + "." + lSectionId, lAmendmentText, lAmendmentTitle});
-                            } else
-                                amendment.additions.add(new String[]{lPartId + "." + lDivisionId + "." + lSectionId, lAmendmentText, lAmendmentTitle});
-                        }
-                        else {
-                            amendment.additions.add(new String[]{lPartId + "." + lDivisionId, lAmendmentText, lAmendmentTitle});
-                            successfullyExecutedCommandMessage(pInteraction);
-                        }
-                    }
+        // Check if part exists
+        assert bill != null;
+        if(!(lPartId < bill.partArrayList.size())) {
+            pInteraction.createImmediateResponder().append("This Part does not exist").respond();
+            return;
+        }
 
-            }
-            else {
-                amendment.additions.add(new String[]{String.valueOf(lPartId), lAmendmentText, lAmendmentTitle});
+        System.out.println("Part Id: " + lPartId);
+
+        // Checks if division exists
+        if(lSlashCommandInteractionOption.getOptionLongValueByName("division_id").isEmpty()) {
+            assert amendment != null;
+            amendment.additions.add(new String[]{String.valueOf(lPartId), lAmendmentText, lAmendmentTitle});
+            successfullyExecutedCommandMessage(pInteraction);
+        }
+
+        int lDivisionId = Math.toIntExact(lSlashCommandInteractionOption.getOptionLongValueByName("division_id").get());
+        System.out.println("Division Id: " + lDivisionId);
+
+        if(lDivisionId < bill.partArrayList.get(lPartId).divisionArrayList.size()) {
+            if(lSlashCommandInteractionOption.getOptionLongValueByName("section_id").isEmpty()) {
+                assert amendment != null;
+                amendment.additions.add(new String[]{lPartId + "." + lDivisionId, lAmendmentText, lAmendmentTitle});
                 successfullyExecutedCommandMessage(pInteraction);
             }
-            bill.amendmentDrafts.set(amendmentDraftId, amendment);
-            bills.put(String.valueOf(bill.messageId), bill);
-            MainQuorum.saveBills();
-            amendment.updateMessage("n", amendmentDraftId, bill);
-        } else
-            pInteraction.createImmediateResponder().append("This Part does not exist").respond();
 
+            int lSectionId = Math.toIntExact(lSlashCommandInteractionOption.getOptionLongValueByName("section_id").get());
+            System.out.println("Section Id: " + lSectionId);
+
+            if(!(lDivisionId < bill.partArrayList.get(lPartId).divisionArrayList.get(lDivisionId).sectionArrayList.size())) {
+                assert amendment != null;
+                amendment.additions.add(new String[]{lPartId + "." + lDivisionId + "." + lSectionId, lAmendmentText, lAmendmentTitle});
+            }
+
+            if(lSlashCommandInteractionOption.getOptionStringValueByName("sub_section_id").isEmpty()) {
+                assert amendment != null;
+                amendment.additions.add(new String[]{lPartId + "." + lDivisionId + "." + lSectionId, lAmendmentText, lAmendmentTitle});
+            }
+
+            String lSubSectionIdString = lSlashCommandInteractionOption.getOptionStringValueByName("sub_section_id").get();
+            System.out.println("Sub-Section Id as String: " + lSubSectionIdString);
+            String[] lSubSectionIdStringArray = lSubSectionIdString.split("\\.");
+            System.out.println("Sub-Section Id as Array: " + Arrays.toString(lSubSectionIdStringArray));
+            int[] lSubSectionIdIntArray = new int[lSubSectionIdStringArray.length];
+            boolean lValid = true;
+            for(int i = 0; i<lSubSectionIdStringArray.length; i++) {
+                try { lSubSectionIdIntArray[i] = Integer.parseInt(lSubSectionIdStringArray[i])-1; }
+                catch (NumberFormatException | NullPointerException e) { lValid = false; }
+            }
+
+            if(lValid) {
+                if(lSubSectionIdIntArray[0] < bill.partArrayList.get(lPartId).divisionArrayList.get(lDivisionId).sectionArrayList.get(lSectionId).subSectionArrayList.size()) {
+                    SubSection lSubSection  = bill.partArrayList.get(lPartId).divisionArrayList.get(lDivisionId).sectionArrayList.get(lSectionId).subSectionArrayList.get(lSubSectionIdIntArray[0]);
+                    boolean lValid2 = true;
+                    for (int i = 1; i < lSubSectionIdIntArray.length &&lValid2; i++) {
+                        assert lSubSection.subSectionArrayList != null;
+                        if (lSubSection.subSectionArrayList.size() <= lSubSectionIdIntArray[i]) {
+                            pInteraction.createImmediateResponder().append("Not a valid Sub-Section.\nError Code: 02").respond();
+                            lValid2 = false;
+                        } else lSubSection = lSubSection.subSectionArrayList.get(lSubSectionIdIntArray[i]); }
+                    if(lValid2) {
+                        assert amendment != null;
+                        amendment.additions.add(new String[]{lPartId + "." + lDivisionId + "." + lSectionId + "." + lSubSectionIdString, lAmendmentText, lAmendmentTitle});
+                    }
+                } else pInteraction.createImmediateResponder().append("Not a valid Sub-Section.\nError Code: 01").respond();
+            }
+        }
+
+        bill.amendmentDrafts.set(amendmentDraftId, amendment);
+        bills.put(String.valueOf(bill.messageId), bill);
+        MainQuorum.saveBills();
+        assert amendment != null;
+        amendment.updateMessage("n", amendmentDraftId, bill);
     }
 }
